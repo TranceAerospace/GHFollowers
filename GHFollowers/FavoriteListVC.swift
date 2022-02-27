850335a275987ba9d9cd82e46eae7abcd13a728d
TranceAerospace <xp46290@gmail.com>
2022-02-27 07:11:11 -0500

    Initial project setup

---
+++ GHFollowers/FavoriteListVC.swift
@@ -0,0 +1,21 @@
+//
+//  FavoriteListVC.swift
+//  GHFollowers
+//
+//  Created by Mark Trance on 2/26/22.
+//
+
+import UIKit
+
+class FavoriteListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+		view.backgroundColor = .systemBlue
+    }
+    
+
+    
+
+}
---
+++ GHFollowers/SearchVC.swift
@@ -0,0 +1,20 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by Mark Trance on 2/26/22.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+		view.backgroundColor = .systemPink
+    }
+    
+
+
+}
