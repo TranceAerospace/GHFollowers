5c421c6fb3d4a3a12ad5f86b511d1477148f51f6
TranceAerospace <xp46290@gmail.com>
2022-03-10 06:57:22 -0500

    Created models for a User and a Follower

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,15 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Mark Trance on 3/10/22.
+//
+
+import Foundation
+
+
+struct Follower: Codable {
+	var login: String
+	var avatarUrl: String
+	
+}
