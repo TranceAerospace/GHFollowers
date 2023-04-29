29e3c22f6cf12570c7861730d17137e8bc80f8c0
Trance <xp46290@gmail.com>
2023-04-29 08:22:24 -0400

    Add search and favorites view controllers in a tab controller.

---
+++ GHFollowers/FavoritesVC.swift
@@ -0,0 +1,19 @@
+//
+//  FavoritesVC.swift
+//  GHFollowers
+//
+//  Created by Mark Trance on 4/28/23.
+//
+
+import UIKit
+
+class FavoritesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+    
+
+}
--- GHFollowers/SearchVC.swift
+++ GHFollowers/SearchVC.swift
@@ -1,19 +1,17 @@
 //
-//  ViewController.swift
+//  SearchVC.swift
 //  GHFollowers
 //
-//  Created by Mark Trance on 4/26/23.
+//  Created by Mark Trance on 4/28/23.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         view.backgroundColor = .systemPink
     }
-
-
 }
-
